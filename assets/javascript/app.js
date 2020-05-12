d98796bbaa7bd3db997b1ee5325b5ac166fb872c
slimzim <slimzim@gmail.com>
2020-05-12 15:59:49 -0500

    got gifs dynamically adding, an 'add to favorites' button added to each gif, start/stop animation functionality is working, added a button to end of the gifs div to request 10 more gifs of that character, could not figure out how to make remove button on favorite gifs work.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,9 @@
 // VARIABLES ==========================================================================
 
+var queryLimit = 10;
 var characterName = ""
 var APIKey = "S43IKOzbb1L394FnVMrdlmpxpaYFVWca";
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + characterName + "&limit=10&offset=0&rating=G&lang=en"
-
+var queryURL = ""
 var tvCharacters = ["Homer Simpson", "Peter Griffin", "Carlton Banks", "Al Bundy", "George Costanza", "Kramer", "Joey Tribiani", "Monica Geller", "Steve Urkel", "Michelle Tanner", "Archie Bunker", "Lucille Ball"]
 
 
@@ -13,7 +13,6 @@ var tvCharacters = ["Homer Simpson", "Peter Griffin", "Carlton Banks", "Al Bundy
 function renderButtons(){
     $(".buttons-container").empty()
     for (var i=0; i < tvCharacters.length; i++){
-        console.log(tvCharacters[i])
         b = $("<button>")
         b.addClass("character-button")
         b.attr("data-name", tvCharacters[i])
@@ -24,41 +23,83 @@ function renderButtons(){
 
 renderButtons()
 
+// MORE GIFS ONCLICK ===============================================================
+$(document).on("click", ".queryLimit-raiser", function(){
+    if (characterName === ($(this).data("name"))) {
+        console.log("if triggered")
+        queryLimit = queryLimit + 10;
+        console.log("limit raised to " + queryLimit)
+    }    
+})
+
 // AJAX CALL TO GIPHY API =============================================================
 
 $(document).on("click", ".character-button", function(){
     $(".gifs-container").empty()
+    $("#instructions").text("Click on a gif to animate it! Click again to stop it.")
 
+    if (characterName !== $(this).data("name")){
+        queryLimit = 10
+    }
+
+    console.log(queryLimit)
     characterName = ($(this).data("name"))
     
-    queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + characterName + "&limit=10&offset=0&rating=G&lang=en"
+    queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + APIKey + "&q=" + characterName + "&limit=" + queryLimit + "offset=0&rating=G&lang=en"
 
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function(response) {
-        console.log("queryURL " + queryURL)
     console.log(response)
    
     for (var i=0; i<response.data.length; i++){
 
-    
+    newGifDiv = $("<div>")
+    newGifDiv.addClass("gif-div text-center")
 
     newGif = $("<img>")
-    // newGif.attr("src", "https://via.placeholder.com/150")
-    newGif.attr("src", response.data[i].embed_url)
-    newGif.appendTo(".gifs-container")
+    // newGif.addClass("card-img-top")
+    newGif.attr("src", response.data[i].images.fixed_height_small_still.url)
+    newGif.attr("data-still", response.data[i].images.fixed_height_small_still.url)
+    newGif.attr("data-animate", response.data[i].images.fixed_height_small.url)
+    newGif.attr("data-state", "still")
+    newGif.addClass("gif")
+    newGif.appendTo(newGifDiv)
 
-    newGifRating = $("<div>")
-    newGifRating.text("Gif Rating: " + response.data[i].rating)
-    newGifRating.appendTo(".gifs-container")
+    newGifTitle = $("<h6>")
+    // newGifTitle.addClass("card-title")
+    newGifTitle.text(response.data[i].title)
+    newGifTitle.appendTo(newGifDiv)
+
+    newGifRating = $("<p>")
+    // newGifRating.addClass("card-text")
+    newGifRating.text("Rating: " + response.data[i].rating)
+    newGifRating.appendTo(newGifDiv)
+
+    newGifFavButton = $("<button>", {text: "Add to Favorites"})
+    newGifFavButton.addClass("fav-button btn btn-primary")
+    newGifFavButton.attr("gifnumber", i)
+    newGifFavButton.attr("img-src", response.data[i].images.fixed_height.url)
+    newGifFavButton.attr("data-title", response.data[i].title)
+    newGifFavButton.appendTo(newGifDiv)
+    
+    newGifDiv.appendTo(".gifs-container")
     }
+
+    moreGifsButton = $("<button>", {text: "Give me 10 more " + characterName + " gifs!"})
+    moreGifsButton.addClass("character-button queryLimit-raiser")
+    moreGifsButton.attr("data-name", characterName)
+    moreGifsButton.appendTo(".gifs-container")
+
+    
+
 })
 })
 
 
-// ADD A CARACTER BUTTON =================================================================
+// ADD A CHARACTER BUTTON =================================================================
 
 $(document).on("click", "#add-character", function(event) {
     event.preventDefault();
@@ -66,3 +107,66 @@ $(document).on("click", "#add-character", function(event) {
     tvCharacters.push(newChar);
     renderButtons();
   });
+
+// ONCLICK TO TOGGLE ANIMATION =============================================================
+
+$(document).on("click", ".gif", function(){
+    var state = $(this).attr("data-state")
+
+    if (state === "still") {
+        $(this).attr("data-state", "animate");
+        $(this).attr("src", $(this).attr("data-animate"));
+      }
+
+      else if (state === "animate") {
+        $(this).attr("data-state", "still");
+        $(this).attr("src", $(this).attr("data-still"))
+      }
+})
+
+
+//  FAVORITE BUTTON ONCLICK ================================================================
+
+var favGifNumber = 0;
+
+$(document).on("click", ".fav-button", function(){
+    console.log($(this).attr("gifnumber"))
+    console.log($(this).attr("img-src"))
+    
+
+    favGifDiv = $("<div>")
+    favGifDiv.addClass("card faves-card text-center")
+
+//remove button classes
+    favGifNumber++;
+    removeValue = "removeValue" + favGifNumber
+    favGifDiv.addClass(removeValue) 
+//======================
+    
+    newFavGif = $("<img>")
+    newFavGif.attr("src", $(this).attr("img-src"))
+    newFavGif.addClass("card-img-top")
+    newFavGif.appendTo(favGifDiv)
+    newFavGifBody = $("<div>")
+    newFavGifBody.addClass("card-title")
+    newFavGifBody.text($(this).attr("data-title"))
+    newFavGifBody.appendTo(favGifDiv)
+    removeFavButton = $("<button>", {text: "Remove"})
+    removeFavButton.addClass("btn btn-primary remove-fav")
+    removeFavButton.attr("favGifNumber", favGifNumber)
+    console.log($(this).attr("favGifNumber"))
+    removeFavButton.appendTo(favGifDiv)
+    favGifDiv.appendTo(".faves-div")
+    
+})
+
+// REMOVE BUTTON ONCLICK ==========================================================
+
+$(document).on("click", ".remove-fav", function(){
+    console.log($(this).attr("favGifNumber"))
+    console.log("removeValue" + favGifNumber)
+    $(".faves-div").remove(".removeValue" + favGifNumber)
+    $(".faves-div").remove(".removeMe")
+    $(".faves-div").remove(".removeValue1")
+})
+
